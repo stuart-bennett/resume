45cf672291df7f06ba78a777758ff9dc85af5957
Stuart Bennett <stuart.bennett@live.co.uk>
2016-02-28 21:16:22 +0000

    Add new data source + layer when showing a location (instead of overwriting a single point

--- src/javascript/map.js
+++ src/javascript/map.js
@@ -3,57 +3,61 @@ import mapbox from "mapbox-gl"
 class Map {
 
   constructor($container, accessToken) {
-    console.log($container);
     mapbox.accessToken = accessToken;
-    console.log("kfjdlskjgfd");
     this.$container = $container;
+    this.locationCount = 0;
   }
 
   init($container) {
     this.map = new mapbox.Map({
       container: this.$container,
       style: "mapbox://styles/stubennett/cijizel74006xb3m1tjsvyk7h",
-      center: [-74.50, 40], // starting position
-      zoom: 9, // starting zoom,
+      center: [-1.8, 52.79], // starting position
+      zoom: 12, // starting zoom,
       width: "100%"
     });
 
     this.map.on("style.load", () => {
+      // this.doAdd("single-point"); 
+    });
+  }
 
-      this.map.addSource('single-point', {
-      	"type": "geojson",
-	"data": {
-	  "type": "FeatureCollection",
-	  "features": []
-	}
-      });
-
-      this.map.addLayer({
-	  "id": "point",
-	  "source": "single-point",
-	  "type": "circle",
-	  "paint": {
-	    "circle-radius": 10,
-	    "circle-color": "#007cbf"
-          }
-        });
+  doAdd(name, color = "#007cbf") {
+   console.log(name);
+    this.map.addSource(name, {
+      "type": "geojson",
+      "data": {
+        "type": "FeatureCollection",
+        "features": []
+      }
+    });
 
 
-      });
+    this.map.addLayer({
+      "id": name + "-point",
+      "source": name,
+      "type": "circle",
+      "paint": {
+        "circle-radius": 10,
+        "circle-color": color
+      }
+    });
   }
 
   markLocation(lat, lng) {
-    console.log("Lat: " + lat);
-    console.log("Long: " + lng);
     let data = {
       "type": "Feature",
       "geometry": {
-	"type": "Point",
-	"coordinates": [lng, lat]
+        "type": "Point",
+        "coordinates": [lng, lat]
       }
     };
-    console.log(data);
-    this.map.getSource("single-point").setData(data);
+
+    let name = "location-" + (++this.locationCount);
+    console.log(lat);
+    console.log(lng);
+    this.doAdd(name);
+    this.map.getSource(name).setData(data);
   }    
   
   focusOn(lat, lng) { 
