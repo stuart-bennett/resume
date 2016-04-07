6fb2143b4d21b53bedac0656a1d67ad6ae68c542
Stuart Bennett <stuart.bennett@live.co.uk>
2016-04-07 22:07:30 +0100

    No longer need this copy task after adding nunjucks templates

--- gulp-tasks/copy.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp"),
-    settings = require("../gulp-settings");
-
-module.exports = function () {
-    return gulp.src(settings.dirs.src + "/**/*.html").pipe(gulp.dest(settings.dirs.build));
-};
-
-module.exports.dependencies = ["clean"];
